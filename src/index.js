537f546bf9fb8ac9f49f790a6855aaeddabe102a
Pratham <prathamparekh16@gmail.com>
2025-02-25 13:38:43 -0500

    Updated server to listen on PORT 8080 for Cloud Run

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 }).catch((err) => {
     console.error(err);
     process.exit(1);
